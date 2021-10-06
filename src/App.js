972862c0d42734c76e1cb76c476b0b31facca930
Suellen Oliveira <dev.suellen@gmail.com>
2021-10-06 03:12:28 -0300

    comecando pagina inicial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <>
-      <h1>App</h1>
       <Home />
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,38 @@
+import styled from "styled-components";
+
 const Home = () => {
-  return <section>Página Inicial</section>;
+  return (
+    <MainHome>
+      <Content>
+        <Button>Começar</Button>
+      </Content>
+    </MainHome>
+  );
 };
 
 export default Home;
+
+const MainHome = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.section``;
+
+const Button = styled.button`
+  background-color: var(--red);
+  height: 50px;
+  width: 270px;
+  border-style: none;
+  border-radius: 20px;
+  font-size: 24px;
+  line-height: 48px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    border: 2px solid #fff;
+  }
+`;
